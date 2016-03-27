c72bbfb50fb6d14733802d57ccb58221f8b3ce1d
Kirich <ivadik2323@gmail.com>
2016-03-27 18:37:23 +1000

    Remove commentaries

--- Controller.cpp
+++ Controller.cpp
@@ -4,38 +4,6 @@
 Controller::Controller(Map& map_){
 	map = map_;
 	gameover = false;
-	//actors.resize(map_.GetMap().size());
-	/*for (int i = 0; i < map.GetMap().size(); i++)
-	{
-		actors[i].resize(map.GetMap()[i].size(), nullptr);
-		for (int j = 0; j < map.GetMap()[i].size(); j++)
-		{
-			switch (map.GetMap()[i][j]) {
-			case KNIGHT_SYMBOL: {
-				if (player == NULL)
-					AddPlayer(new Knight(100, j, i));
-				else
-					throw(std::runtime_error("Too many knights"));
-				break;
-			}
-			case PRINCESS_SYMBOL: 
-			{
-				if (princess == NULL)
-					AddPrincess(new Princess(100, j, i));
-				else
-					throw(std::runtime_error("Too many princesses"));
-				break;
-			}
-			case ZOMBIE_SYMBOL: AddCharacter(new Zombie(10, j, i));
-				break;
-			case DRAGON_SYMBOL: AddCharacter(new Dragon(50, j, i));
-				break;
-			case MEDKIT_SYMBOL: AddItem(new Medkit(j, i));
-				break;
-			}
-		}
-	}*/
-
 }
 
 void Controller::Start() {
@@ -50,32 +18,6 @@ void Controller::Start() {
 			Lose();
 	}
 }
-
-//void Controller::AddPlayer(Actor * c)
-//{
-//	characters.push_front(c);
-//	player = c;
-//	actors[c->position().second][c->position().first] = c;
-//}
-//
-//void Controller::AddPrincess(Actor * c)
-//{
-//	//characters.push_back(c);
-//	princess = c;
-//	actors[c->position().second][c->position().first] = c;
-//}
-//
-//void Controller::AddCharacter(Actor * c)
-//{
-//	characters.push_back(c);
-//	actors[c->position().second][c->position().first] = c;
-//}
-
-//void Controller::AddItem(Actor * c)
-//{
-//	actors[c->position().second][c->position().first] = c;
-//}
-
 void Controller::Win(void)
 {
 	gameover = true;
@@ -90,14 +32,6 @@ void Controller::Lose(void)
 	std::cout << ";_; GAME OVER ;_;" << std::endl;
 }
 
-//auto Controller::Find(std::pair<int, int> pos)
-//{
-	/*for (auto iter = characters.begin(); iter != characters.end(); iter++)
-		if ((*iter)->position() == pos)
-			return iter;
-	return characters.end();*/
-//}
-
 void Controller::NextMove()
 {
 	map.ClearActed();
@@ -111,59 +45,10 @@ void Controller::NextMove()
 				map.GetMap()[i][j]->Act(map);
 		}
 	}
-	//for (auto i = characters.begin(); i != characters.end(); i++)
-	//{
-	//	if (((Character*)(*i))->HitPoint() <= 0)
-	//	{
-	//		if (i != characters.begin())
-	//		{
-	//			delete(*i);
-	//			characters.erase(i--);
-	//		}
-	//		continue;
-	//	}
-
-	//	std::pair<int, int> pos = ((Character*)(*i))->Move(map);
-	//	std::pair<int, int> started_pos = (*i)->position();
-
-	//	if (pos == std::pair<int, int>(-1, -1) || pos == started_pos)
-	//		continue;
-
-	//	Actor* target = actors[pos.second][pos.first];
-
-	//	if (target == nullptr)
-	//	{
-	//		(*i)->replace(pos, map);
-	//		actors[pos.second][pos.first] = (*i);
-	//		actors[started_pos.second][started_pos.first] = nullptr;
-	//	}
-	//	else
-	//	{
-	//		target->Collide(*i);
-	//		std::pair<int, int> new_pos = (*i)->position();
-	//		if (started_pos != new_pos)
-	//		{
-	//			//if (actors[started_pos.second][started_pos.first] != nullptr)
-	//				//delete(actors[started_pos.second][started_pos.first]);
-	//			actors[new_pos.second][new_pos.first] = (*i);
-	//			map.SetChar(new_pos, (*i)->Symbol());
-	//			actors[started_pos.second][started_pos.first] = nullptr;
-	//			map.SetChar(started_pos, '.');
-	//		}
-	//	}
-	//}
-
 }
 
 void Controller::ShowMap()
 {
-	//map.SetChar(princess->position(), princess->Symbol()); // показать принцессу
-
-	//for (auto i = characters.begin(); i != characters.end(); i++)
-	//	map.SetChar((*i)->position(), (*i)->Symbol());     // показать монстров
-
-	//map.SetChar(player->position(), player->Symbol());     //  показать игрока
-
 	system("CLS");
 	printf("%d HP\n", ((Character*)map.GetPlayer())->HitPoint());  // показать HP
 	map.Display();
